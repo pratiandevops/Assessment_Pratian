44b403b3a36a709923cb3ff28481dadfb40ac823
senapatisantosh <santoshkumarsenapati1993@gmail.com>
2019-12-05 14:48:46 +0530

    Card Cmponent Fix

--- src/app/shared-modules/shared-modules.module.ts
+++ src/app/shared-modules/shared-modules.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedModulesRoutingModule } from './shared-modules-routing.module';
-import { CardComponent } from './components/card/card.component';
 
 
 @NgModule({
-  declarations: [CardComponent],
   imports: [
     CommonModule,
     SharedModulesRoutingModule
