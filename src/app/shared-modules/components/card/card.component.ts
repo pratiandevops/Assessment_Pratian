8902f3cef7c96d4ac0bb2956c604750fc5d4ac01
senapatisantosh <santoshkumarsenapati1993@gmail.com>
2019-12-05 14:36:24 +0530

    Card Issues Resolved

--- src/app/shared-modules/components/card/card.component.ts
+++ src/app/shared-modules/components/card/card.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-card',
+  selector: 'card-component',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
