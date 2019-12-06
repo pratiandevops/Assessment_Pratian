146fac5a1057756381e32718792d00fa426cc122
senapatisantosh <santoshkumarsenapati1993@gmail.com>
2019-12-06 12:56:12 +0530

    Routes Addded

--- src/app/shared-modules/shared-modules-routing.module.ts
+++ src/app/shared-modules/shared-modules-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: AuthenticationComponent },
+  { path: 'registration', component: RegistrationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
